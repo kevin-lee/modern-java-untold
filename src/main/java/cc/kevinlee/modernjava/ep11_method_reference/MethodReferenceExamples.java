f971e64bfb42632b583a45b95ab3f4d1dcdd6921
Kevin Lee <kevin.code@lckymn.com>
2016-01-10 22:50:17 +1100

    - Method를 data처럼 쓸수 있는지 증명

--- src/main/java/cc/kevinlee/modernjava/ep11_method_reference/MethodReferenceExamples.java
+++ src/main/java/cc/kevinlee/modernjava/ep11_method_reference/MethodReferenceExamples.java
@@ -3,6 +3,7 @@ package cc.kevinlee.modernjava.ep11_method_reference;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static java.util.stream.Collectors.toList;
 
@@ -55,6 +56,112 @@ public class MethodReferenceExamples {
             .anyMatch(x -> x.equals("c"))
     );
 
+    System.out.println("\n=========================================");
+    System.out.println("methodReference03();");
+    System.out.println("-----------------------------------------");
+    methodReference03();
+  }
+
+  private static void methodReference03() {
+    /* First Class Function */
+    /**
+     * A function can be passed as a parameter to another function.
+     */
+    /* Using Lambda Expression
+     */
+    System.out.println(testFirstClassFunction1(3, i -> String.valueOf(i * 2)));
+    /*
+     * Using Method Reference
+     */
+    System.out.println(testFirstClassFunction1(3, MethodReferenceExamples::doubleThenToString));
+
+    /**
+     * A function can be returned as the result of another function.
+     */
+    /*
+     * Using Lambda Expression
+     */
+    final Function<Integer, String> fl = getDoubleThenToStringUsingLambdaExpression();
+    final String resultFromFl = fl.apply(3);
+    System.out.println(resultFromFl);
+    /*
+     * Using Method Reference
+     */
+    final Function<Integer, String> fmr = getDoubleThenToStringUsingMethodReference();
+    final String resultFromFmr = fmr.apply(3);
+    System.out.println(resultFromFmr);
+
+    System.out.println("\n-----------------------------------------");
+    /**
+     * A function can be stored in the data structure.
+     */
+    /*
+     * Using Lambda Expression
+     */
+    final List<Function<Integer, String>> fsL = Arrays.asList(i -> String.valueOf(i * 2));
+    for (final Function<Integer, String> f : fsL) {
+      final String result = f.apply(3);
+      System.out.println(result);
+    }
+    /*
+     * Using Method Reference
+     */
+    final List<Function<Integer, String>> fsMr = Arrays.asList(MethodReferenceExamples::doubleThenToString);
+    for (final Function<Integer, String> f : fsMr) {
+      final String result = f.apply(3);
+      System.out.println(result);
+    }
+
+    System.out.println("\n-----------------------------------------");
+    /*
+     * Using Lambda Expression
+     */
+    final Function<Integer, String> fl2 = i -> String.valueOf(i * 2);
+    final String resultFl2 = fl2.apply(5);
+    System.out.println(resultFl2);
+
+    /*
+     * Method Reference
+     */
+    final Function<Integer, String> fmr2 = MethodReferenceExamples::doubleThenToString;
+    final String resultFmr2 = fmr2.apply(5);
+    System.out.println(resultFmr2);
+
+    System.out.println("\n-----------------------------------------");
+    /*
+     * Both Lambda Expression and Method Reference
+     */
+    final List<Function<Integer, String>> fsBoth =
+        Arrays.asList(
+            i -> String.valueOf(i * 2),
+            MethodReferenceExamples::doubleThenToString,
+            MethodReferenceExamples::addHashPrefix
+        );
+
+    for (final Function<Integer, String> f : fsBoth) {
+      final String result = f.apply(7);
+      System.out.println(result);
+    }
+  }
+
+  private static String doubleThenToString(int i) {
+    return String.valueOf(i * 2);
+  }
+
+  private static String addHashPrefix(int number) {
+    return "#" + number;
+  }
+
+  private static String testFirstClassFunction1(int n, Function<Integer, String> f) {
+    return "The result is " + f.apply(n) + ".";
+  }
+
+  private static Function<Integer, String> getDoubleThenToStringUsingLambdaExpression() {
+    return i -> String.valueOf(i * 2);
+  }
+
+  public static Function<Integer,String> getDoubleThenToStringUsingMethodReference() {
+    return MethodReferenceExamples::doubleThenToString;
   }
 }
 
